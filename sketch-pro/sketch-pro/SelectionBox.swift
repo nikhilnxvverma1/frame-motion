633e5a6633e47392e6ae948663d2ffc73110605d
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-09-25 21:20:08 +0530

    Selection box view for currently selected item

---
+++ sketch-pro/sketch-pro/SelectionBox.swift
@@ -0,0 +1,19 @@
+//
+//  SelectionBox.swift
+//  sketch-pro
+//
+//  Created by Nikhil Verma on 9/25/17.
+//  Copyright © 2017 Nikhil Verma. All rights reserved.
+//
+
+import Cocoa
+
+class SelectionBox: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
