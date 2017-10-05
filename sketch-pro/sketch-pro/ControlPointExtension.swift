6279b7f3dec1b086c6abc1854fb0afe730291a0d
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-10-05 21:41:13 +0530

    Sketchy implementation of dotted line view (Control point extensions)

--- sketch-pro/sketch-pro/ControlPointExtension.swift
+++ sketch-pro/sketch-pro/ControlPointExtension.swift
@@ -18,7 +18,22 @@ class ControlPointExtension: NSView {
 
         // Drawing code here.
 		
-		//TODO dotted line
-    }
-    
+		// dotted line
+		let aPath = NSBezierPath()
+		
+		aPath.move(to: CGPoint(x:anchorPoint.x, y:anchorPoint.y))
+		
+		aPath.line(to: CGPoint(x:controlPoint.x, y:controlPoint.y))
+		
+		//Keep using the method addLineToPoint until you get to the one where about to close the path
+		
+		aPath.close()
+		
+		//If you want to stroke it with a red color
+		NSColor.red.set()
+		aPath.stroke()
+		//If you want to fill it as well
+		aPath.fill()
+	}
+	
 }
