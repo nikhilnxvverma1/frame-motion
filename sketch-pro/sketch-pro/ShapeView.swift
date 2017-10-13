186bc49ac668c382d72f902c3d10370b2602f3ca
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-10-13 22:04:22 +0530

    Minor formatting of bezier curve expression

--- sketch-pro/sketch-pro/ShapeView.swift
+++ sketch-pro/sketch-pro/ShapeView.swift
@@ -71,6 +71,8 @@ class ShapeView: NSView {
 			i+=1
 		}
 		
+		
+		// TODO: get color and stroke from model
 		NSColor.darkGray.setFill()
 		path.stroke()
 		NSColor.lightGray.setFill()
@@ -85,24 +87,87 @@ class ShapeView: NSView {
 		var hx = 0.0
 		var hy = 0.0
 		
-		for point in points{
+		var i = 0
+		while i < points.count-1 {
 			
-			if(point.x<CGFloat(lx)){
-				lx = Double(point.x)
+			let point = points[i]
+			let nextPoint = points[i+1]
+			
+//			let nextPointCoords = NSPoint(x: nextPoint.x, y: nextPoint.y)
+			
+//			if( point.forwardControlPoint != nil && nextPoint.backwardControlPoint != nil){
+//				
+//				let controlPoint1Coords = NSPoint(x: point.forwardControlPoint.x, y: point.forwardControlPoint.y)
+//				let controlPoint2Coords = NSPoint(x: nextPoint.backwardControlPoint.x, y: nextPoint.backwardControlPoint.y)
+//				
+//			}else if( point.forwardControlPoint == nil && nextPoint.backwardControlPoint != nil){
+//				
+//				let controlPoint1Coords = NSPoint.zero
+//				let controlPoint2Coords = NSPoint(x: nextPoint.backwardControlPoint.x, y: nextPoint.backwardControlPoint.y)
+//				
+//			}else if( point.forwardControlPoint != nil && nextPoint.backwardControlPoint == nil){
+//				
+//				let controlPoint1Coords = NSPoint(x: point.forwardControlPoint.x, y: point.forwardControlPoint.y)
+//				let controlPoint2Coords = NSPoint.zero
+//				
+//			}else if( point.forwardControlPoint == nil && nextPoint.backwardControlPoint == nil){
+//				
+//				let controlPoint1Coords = NSPoint.zero
+//				let controlPoint2Coords = NSPoint.zero
+//				
+//			}
+			
+			
+			// run through this curve to search for extremas
+			var t = 0.0
+			while t <= 1 {
+				
+				// Bezier curve equation
+				// (1-t)^3*P0 * 3(1-t)^2*t*P1 + 3(1-t)*t^2*P2 + t^3*P3
+				let x = (1-t)*(1-t)*(1-t)*point.x * 3*(1-t)*(1-t)*t*point.forwardControlPoint.x + 3*(1-t)*t*t*nextPoint.backwardControlPoint.x + t*t*t*nextPoint.x
+				let y = (1-t)*(1-t)*(1-t)*point.y * 3*(1-t)*(1-t)*t*point.forwardControlPoint.y + 3*(1-t)*t*t*nextPoint.backwardControlPoint.y + t*t*t*nextPoint.y
+//				let y = (1-t)^3*point.y * 3*(1-t)^2*t*point.forwardControlPoint.y + 3*(1-t)*t^2*nextPoint.backwardControlPoint.y + t^3*nextPoint.y
+
+				if(x<CGFloat(lx)){
+					lx = Double(x)
+				}
+				
+				if(y<CGFloat(ly)){
+					ly = Double(y)
+				}
+				
+				if(x>CGFloat(hx)){
+					hx = Double(x)
+				}
+				
+				if(y>CGFloat(hy)){
+					hy = Double(y)
+				}
+				t = t + 0.01
 			}
 			
-			if(point.y<CGFloat(ly)){
-				ly = Double(point.y)
-			}
 			
-			if(point.x>CGFloat(hx)){
-				hx = Double(point.x)
-			}
-			
-			if(point.y>CGFloat(hy)){
-				hy = Double(point.y)
-			}
+			i+=1
 		}
+		
+//		for point in points{
+//			
+//			if(point.x<CGFloat(lx)){
+//				lx = Double(point.x)
+//			}
+//			
+//			if(point.y<CGFloat(ly)){
+//				ly = Double(point.y)
+//			}
+//			
+//			if(point.x>CGFloat(hx)){
+//				hx = Double(point.x)
+//			}
+//			
+//			if(point.y>CGFloat(hy)){
+//				hy = Double(point.y)
+//			}
+//		}
 		
 		self.frame.origin.x = CGFloat(lx)
 		self.frame.origin.y = CGFloat(ly)
