3c068bac6f424d85b9a6b22636550343e5fd8cfe
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-12-02 23:46:44 +0530

    Translate command for supporting undo redo of movement

---
+++ sketch-pro/sketch-pro/Translate.swift
@@ -0,0 +1,33 @@
+//
+//  Translate.swift
+//  sketch-pro
+//
+//  Created by Nikhil Verma on 12/2/17.
+//  Copyright © 2017 Nikhil Verma. All rights reserved.
+//
+
+import Cocoa
+
+class Translate: NSObject, Command {
+	var shapeView : ShapeView!
+	var document : Document!
+	var artboardView : ArtboardView!
+	var difference : NSPoint!
+	
+	init(shapeView : ShapeView,artboardView:ArtboardView,document : Document, difference : NSPoint) {
+		super.init()
+		self.document = document
+		self.artboardView = artboardView
+		self.shapeView = shapeView
+		self.difference = difference
+	}
+	
+	func execute(){
+		
+	}
+	
+	func unexecute(){
+		
+	}
+	
+}
