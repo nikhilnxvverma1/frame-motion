00447b9fdf1e432e91840fe756356d719d05d29a
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-01-04 23:51:39 +0530

    Duplicated implementation of text view's rectangle

--- sketch-pro/sketch-pro/TextTool.swift
+++ sketch-pro/sketch-pro/TextTool.swift
@@ -13,6 +13,7 @@ class TextTool: NSObject, ArtboardHandler, Tool{
 	
 	var document : Document!
 	var textView : TextView!
+	var originalPoint: NSPoint!
 	
 	init (_ document : Document){
 		self.document = document
@@ -22,17 +23,46 @@ class TextTool: NSObject, ArtboardHandler, Tool{
 	
 	func mouseDown(with event: NSEvent,artboardView: ArtboardView){
 	
+		textView = TextView()
+		
+		let localPoint = artboardView.convert(event.locationInWindow, from : nil)
+		originalPoint=localPoint
+		textView.frame.origin.x=localPoint.x
+		textView.frame.origin.y=localPoint.y
+		
+		
+		artboardView.addSubview(textView)
 		
 	}
 	
 	func mouseDragged(with event: NSEvent,artboardView: ArtboardView){
 		
+		let localPoint = artboardView.convert(event.locationInWindow, from : nil)
+		//width
+		if(originalPoint.x < localPoint.x){
+			textView.frame.size.width = localPoint.x - originalPoint.x
+		}else{
+			textView.frame.size.width =  originalPoint.x - localPoint.x
+			textView.frame.origin.x = localPoint.x
+		}
+		
+		//height
+		if(originalPoint.y < localPoint.y){
+			textView.frame.size.height = localPoint.y - originalPoint.y
+			
+		}else{
+			textView.frame.size.height =  originalPoint.y - localPoint.y
+			textView.frame.origin.y = localPoint.y
+		}
 		
 	}
 	
 	func mouseUp(with event: NSEvent,artboardView: ArtboardView){
-		
-	
+
+		// TODO : Create Text Command
+//		let command = CreateRectangle(textView,artboardView,document: self.document)
+//		self.document.workspace.pushCommand(command: command, executeBeforePushing: false)
+		self.document.workspace.itemList.add(textView)
 		
 	}
 	
